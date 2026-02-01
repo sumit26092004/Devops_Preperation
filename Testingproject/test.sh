aca576bd8b784ac4e5bebb1a89896874f011f1d7
Sumit <sumitrai41716@gmail.com>
2026-02-01 22:02:53 +0530

    Testing projects pushed

---
+++ Testingproject/test.sh
@@ -0,0 +1,33 @@
+root@LAPTOP-FT58RM1J:/var/lib/jenkins# ls -ltrh
+total 124K
+-rw-r--r--  1 jenkins jenkins   64 Oct  3 14:37 secret.key
+-rw-r--r--  1 jenkins jenkins    0 Oct  3 14:37 secret.key.not-so-secret
+-rw-r--r--  1 jenkins jenkins  171 Oct  3 14:37 jenkins.telemetry.Correlator.xml
+drwxr-xr-x  2 jenkins jenkins 4.0K Oct  3 14:37 userContent
+-rw-------  1 jenkins jenkins 1.7K Oct  3 14:46 identity.key.enc
+-rw-r--r--  1 jenkins jenkins  370 Oct  3 14:46 hudson.plugins.git.GitTool.xml
+-rw-r--r--  1 jenkins jenkins  184 Oct  3 14:50 jenkins.model.JenkinsLocationConfiguration.xml
+-rw-r--r--  1 jenkins jenkins    7 Oct  3 14:50 jenkins.install.UpgradeWizard.state
+-rw-r--r--  1 jenkins jenkins  364 Oct  3 14:51 io.jenkins.plugins.thememanager.ThemeManagerPageDecorator.xml
+-rw-r--r--  1 jenkins jenkins  418 Oct  3 14:51 io.jenkins.plugins.pipelinegraphview.PipelineGraphViewConfiguration.xml
+-rw-r--r--  1 jenkins jenkins   95 Oct  3 14:51 jenkins.console.ConsoleUrlProviderGlobalConfiguration.xml
+-rw-r--r--  1 jenkins jenkins 1.8K Oct  3 20:01 credentials.xml
+drwxr-xr-x  3 jenkins jenkins 4.0K Oct  3 20:02 nodes
+drwxr-xr-x  4 jenkins jenkins 4.0K Oct  3 20:09 logs
+drwxr-xr-x 92 jenkins jenkins  12K Oct  3 20:15 plugins
+drwxr-xr-x  5 jenkins jenkins 4.0K Oct  4 11:14 jobs
+drwxr-xr-x  3 jenkins jenkins 4.0K Oct  4 11:16 caches
+-rw-r--r--  1 jenkins jenkins  258 Nov 22 19:07 queue.xml.bak
+drwx------  2 jenkins jenkins 4.0K Nov 22 19:08 secrets
+-rw-r--r--  1 jenkins jenkins  357 Nov 22 19:08 jenkins.security.apitoken.ApiTokenPropertyConfiguration.xml
+drwxr-xr-x  3 jenkins jenkins 4.0K Nov 22 19:08 users
+drwxr-xr-x  3 jenkins jenkins 4.0K Jan 29 23:25 fingerprints
+-rw-r--r--  1 jenkins jenkins  156 Feb  1 18:47 hudson.model.UpdateCenter.xml
+-rw-r--r--  1 jenkins jenkins 1.1K Feb  1 18:47 nodeMonitors.xml
+-rw-r--r--  1 jenkins jenkins    7 Feb  1 18:47 jenkins.install.InstallUtil.lastExecVersion
+-rw-r--r--  1 jenkins jenkins 1.7K Feb  1 18:47 config.xml
+drwxr-xr-x  2 jenkins jenkins 4.0K Feb  1 18:47 updates
+drwxr-xr-x  7 jenkins jenkins 4.0K Feb  1 19:32 workspace
+-rw-r--r--  1 jenkins jenkins   46 Feb  1 19:35 org.jenkinsci.plugins.workflow.flow.FlowExecutionList.xml
+-rw-r--r--  1 jenkins jenkins  258 Feb  1 19:36 queue.xml
+root@LAPTOP-FT58RM1J:/var/lib/jenkins# 
